22e337816ae372cd907f0209fc8b189aef6f997f
JasonChou <swchou524@gmail.com>
2022-09-29 22:22:28 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+ <div class="container">
+  <h1>Hello Vue!</h1>
+ </div>
+</template>
+
+<style lang="scss" scoped>
+  .container {
+    h1 {
+      color: red;
+    }
+  }
+</style>
