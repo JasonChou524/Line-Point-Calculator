547f5a446aa0a70b0b5d8b81b4be8a9e942d33f7
JasonChou <swchou524@gmail.com>
2022-09-30 12:27:05 +0800

    feat: 新增計算功能、初步切版

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,68 @@
 <script setup>
-
+  import { computed } from "@vue/reactivity";
+  import {ref} from "vue"
+  const total = ref()
+  const usePoint = computed(() => {
+    return (total.value - Math.ceil((Math.round(total.value * 0.015) - 0.5) / 0.015))
+  })
+  const needPay = computed(() => {
+    return (Math.ceil((Math.round(total.value * 0.015) - 0.5) / 0.015))
+  })
+  const getPonit = computed(() => {
+    return (Math.round(total.value * 0.015))
+  })
 </script>
 
 <template>
  <div class="container">
-  <h1>Hello Vue!</h1>
+  <div class="wrapper">
+    <h1>點數計算機</h1>
+    <input v-model="total" type="number" placeholder="請輸入金額">
+    <ul>
+      <li>花費點數：{{ usePoint }} 點</li>
+      <li>支付金額：{{ needPay }} 元</li>
+      <li>獲得點數：{{ getPonit }} 點</li>
+    </ul>
+  </div>
  </div>
 </template>
 
 <style lang="scss" scoped>
   .container {
+    background-color: #FFCAE7;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .wrapper{
+    box-sizing: border-box;
+    width:  250px;
+    height: 300px;
+    padding: 50px 0 0 40px;
+    background-color: #fff;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 72px;
+    display: flex;
+    flex-direction: column;
+    // align-items: center;
     h1 {
-      color: red;
+      color: #EE7179;
+      text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+      margin-bottom: 10px;
+    }
+    input {
+      box-sizing: border-box;
+      width: 150px;
+      height: 30px;
+      padding: 5px;
+      margin-bottom: 10px;
+    }
+    ul {
+      list-style-type: none;
+      li {
+        margin-bottom: 5px;
+      }
     }
   }
 </style>
